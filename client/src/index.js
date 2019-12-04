228b53c38ab23ed828e0983b518e3a6683a33d8c
hitesh <hiteshchauhan.669@gmail.com>
2019-12-04 22:24:48 +0530

    feat: Add material-ui react library for generic components
    Resolves: #5

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './IndexStyle/index.module.scss';
 import App from './Container/App/App';
-import * as serviceWorker from './serviceWorker';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['Montserrat', 'sans-serif'].join(','),
+  },
+});
+const app = (
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(app, document.getElementById('root'));
