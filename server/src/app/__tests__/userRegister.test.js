1e089aabd81092260601e1bacbd27fa015a5a2b1
Koushik Mohan <koushikmohan1996@gmail.com>
2019-12-17 14:52:49 +0530

    Fix: Add correct assertions in user registration test.
    Fixes #47

--- server/src/app/__tests__/userRegister.test.js
+++ server/src/app/__tests__/userRegister.test.js
@@ -18,17 +18,9 @@ describe('POST /users/register', () => {
 
   const data = {
     name: 'Aditya Vyas',
-    email: 'cybertron612@gmail.com',
+    email: 'vyasaditya41@live.com',
     password: 'abcd1234',
   };
-  test('respond with 200 created', async () => {
-    await req
-      .post('/users/register')
-      .send(data)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
 
   test('respond with error to be false', async () => {
     await req
@@ -36,7 +28,7 @@ describe('POST /users/register', () => {
       .send(data)
       .set('Accept', 'application/json')
       .expect((res) => {
-        res.body.error = false;
+        expect(res.body.message).toBe('Account Created!');
       });
   });
 
@@ -50,7 +42,58 @@ describe('POST /users/register', () => {
       })
       .set('Accept', 'application/json')
       .expect((res) => {
-        res.body.message = 'User is already registered';
+        expect(res.body.error).toBe('User is already registered');
+      });
+  });
+
+  test('respond with 200 created', async () => {
+    await req
+      .post('/users/login')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.error).toBe(null);
+      });
+  });
+
+  test('Email Not Found, User doesn\'t Exist', async () => {
+    await req
+      .post('/users/login')
+      .send({
+        email: 'vyasaditya42@live.com',
+        password: 'abcd@1234',
+      })
+      .set('Accept', 'application/json')
+      .expect((res) => {
+        expect(res.body.error).toBe('Email Not Found, User doesn\'t Exist!');
+      });
+  });
+
+  test('Password is incorrect, Please Try Again', async () => {
+    await req
+      .post('/users/login')
+      .send({
+        email: 'vyasaditya41@live.com',
+        password: 'abcd@1234',
+      })
+      .set('Accept', 'application/json')
+      .expect((res) => {
+        expect(res.body.error).toBe('Password is incorrect, Please Try Again!');
+      });
+  });
+
+  test('respond with User is already registered', async () => {
+    await req
+      .post('/users/login')
+      .send({
+        email: 'vyasaditya41@live.com',
+        password: 'abcd1234',
+      })
+      .set('Accept', 'application/json')
+      .expect((res) => {
+        expect(res.body.error).toBe(null);
       });
   });
 });
