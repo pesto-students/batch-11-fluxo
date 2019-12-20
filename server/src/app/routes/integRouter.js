14b74174f6c49f8a216f51f0070a1dbf49080a86
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-20 19:21:39 +0530

    fix: Add Github Router to Integrate

--- server/src/app/routes/integRouter.js
+++ server/src/app/routes/integRouter.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import gitconfig from '../../thirdparty/githubAPI/config';
 
 const router = express.Router();
 
 router.get('/slack', (req, res) => {
   const { id: userId } = req.userData;
   res.redirect(`/tp/slack/integrate/${userId}`);
+});
+
+router.get('/github', (req, res) => {
+  const { id: userId } = req.userData;
+  res.redirect(`${gitconfig.install_url}${userId}`);
 });
 
 export default router;
